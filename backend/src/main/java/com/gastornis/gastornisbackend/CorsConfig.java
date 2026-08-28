3bb5ab16d4909036b6876f79257f0a6d46e18e45
Aravindhan-M02 <aravindhan.m0099@gmail.com>
2026-08-28 22:37:50 +0530

    Fix CORS for production frontend

--- backend/src/main/java/com/gastornis/gastornisbackend/CorsConfig.java
+++ backend/src/main/java/com/gastornis/gastornisbackend/CorsConfig.java
@@ -10,7 +10,6 @@ public class CorsConfig {
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
-
         return new WebMvcConfigurer() {
 
             @Override
@@ -28,7 +27,8 @@ public class CorsConfig {
                                 "DELETE",
                                 "OPTIONS"
                         )
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
