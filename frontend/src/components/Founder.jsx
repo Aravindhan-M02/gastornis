c630a56c11d28725e33d0a7b4ed05ef267e2cfd5
Aravindhan-M02 <aravindhan.m0099@gmail.com>
2026-09-15 03:34:21 +0530

    CEO commit

--- frontend/src/components/Founder.jsx
+++ frontend/src/components/Founder.jsx
@@ -164,7 +164,7 @@ function Founder() {
 
 
                                 <span className="founder-role">
-                                    FOUNDER & LEAD DEVELOPER
+                                    FOUNDER & CEO
                                 </span>
 
                             </div>
